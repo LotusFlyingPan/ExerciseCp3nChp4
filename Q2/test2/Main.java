008e24cdba0a509de796c89b02cee9f1ffc0b81a
HyGlobalHD <GamerXzavier@gmail.com>
2020-06-04 09:38:30 +0800

    Create Main.java

---
+++ Q2/test2/Main.java
@@ -0,0 +1,12 @@
+package test2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		double[] lel = {7, 8};
+		LongJump l = new LongJump("1145", "Jehan Miskin", "MLK", lel);
+		System.out.println(l.toString());
+	}
+
+}
