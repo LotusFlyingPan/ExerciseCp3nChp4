e25c26178dfef58df7fc76d2cc8e717a908bb28b
HyGlobalHD <GamerXzavier@gmail.com>
2020-06-04 09:10:09 +0800

    Create Main.java

---
+++ Q1/test2/Main.java
@@ -0,0 +1,13 @@
+package test2;
+
+public class Main {
+
+	public static void main(String[] args) {
+		Cylinder c = new Cylinder();
+		c.setRadius(3);
+		c.setHeight(8);
+		System.out.println("Volume of cylinder: " + c.caclVolume());
+
+	}
+
+}
