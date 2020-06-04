29b85ddca64e2c50343f235c361b7ff9c40b8fad
HyGlobalHD <GamerXzavier@gmail.com>
2020-06-04 10:03:17 +0800

    Create Main.java

---
+++ Q4/test2/Main.java
@@ -0,0 +1,11 @@
+package test2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		Room r = new Room("Tioga Deluxe Suite", 50);
+		System.out.println(r.calculateDeposit());
+	}
+
+}
