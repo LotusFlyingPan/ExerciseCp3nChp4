937243029b9fa47e84518e454fe08cf14fc3d03e
HyGlobalHD <GamerXzavier@gmail.com>
2020-06-04 09:55:30 +0800

    Create Main.java

---
+++ Q3/test2/Main.java
@@ -0,0 +1,11 @@
+package test2;
+
+public class Main {
+
+	public static void main(String[] args) {
+
+		MastersProgram mp = new MastersProgram("Mixed mode");
+		System.out.println(mp.calculateTotalFee());
+	}
+
+}
